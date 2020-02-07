446783155a618e54ee1ac502639127fc2283fe35
ZeroKelien <59883071+ZeroKelien@users.noreply.github.com>
2020-02-07 18:09:01 -0500

    Added the project

---
+++ New Beginnings/Assets/Mouse.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouse : MonoBehaviour
+    
+{
+    public float MouseSens = 100f;
+
+    public Transform Playerbody;
+    float xRotation = 0f;
+    
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * MouseSens * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * MouseSens * Time.deltaTime;
+        Playerbody.Rotate(Vector3.up * mouseX);
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+       
+
+    }
+}
