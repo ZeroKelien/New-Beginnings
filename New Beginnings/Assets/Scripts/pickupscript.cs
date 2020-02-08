890a5ef8f72cbe5df1ccc5ec04e48ab596b0a012
ZeroKelien <59883071+ZeroKelien@users.noreply.github.com>
2020-02-08 15:16:43 -0500

    Added Trees + Collectable Flowers

---
+++ New Beginnings/Assets/Scripts/pickupscript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickupscript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter (Collider other)
+    {
+        if(other.name == "Character")
+        {
+            other.GetComponent<scoringsystem>().points++;
+                  //Add 1 to Points
+            Destroy(gameObject); // This Destroys things.
+     
+		}
+	}
+
+}
---
+++ New Beginnings/Assets/Scripts/scoringsystem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scoringsystem : MonoBehaviour
+{
+    public int points = 0;
+    public AudioSource collectSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnGUI()
+    {
+        collectSound.Play();
+        GUI.Label(new Rect(10,10,300,300), "Score : " + points);    
+
+	}
+}
